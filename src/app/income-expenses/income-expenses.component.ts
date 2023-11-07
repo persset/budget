adfd1ab6994d977a6070c282a2f536f7a829f84a
Jose Victor Persset <jpersset@gmail.com>
2023-11-07 10:38:20 -0300

    Add: Setup income table at income/expenses page

--- src/app/income-expenses/income-expenses.component.ts
+++ src/app/income-expenses/income-expenses.component.ts
@@ -2,48 +2,23 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTabsModule } from '@angular/material/tabs';
 import { HeaderComponent } from '../header/header.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
-const USER_DATA = [
-  {
-    week1: '100',
-    week2: '100',
-    week3: '100',
-    week4: '100',
-  },
+export interface PeriodicElement {
+  week: string;
+  value: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { week: 'Week 1', value: 100 },
+  { week: 'Week 2', value: 100 },
+  { week: 'Week 3', value: 100 },
+  { week: 'Week 4', value: 100 },
 ];
-
-const COLUMNS_SCHEMA = [
-  {
-    key: 'week1',
-    type: 'number',
-    label: 'Week 1',
-  },
-  {
-    key: 'week2',
-    type: 'number',
-    label: 'Week 2',
-  },
-  {
-    key: 'week3',
-    type: 'number',
-    label: 'Week 3',
-  },
-  {
-    key: 'week4',
-    type: 'number',
-    label: 'Week 4',
-  },
-  {
-    key: 'isEdit',
-    type: 'isEdit',
-    label: '',
-  },
-];
-
 @Component({
   selector: 'app-income-expenses',
   standalone: true,
@@ -51,16 +26,17 @@ const COLUMNS_SCHEMA = [
     CommonModule,
     MatTabsModule,
     HeaderComponent,
-    MatTableModule,
-    MatFormFieldModule,
-    MatInputModule,
     FormsModule,
+    MatToolbarModule,
+    MatTableModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   templateUrl: './income-expenses.component.html',
   styleUrls: ['./income-expenses.component.css'],
 })
 export class IncomeExpensesComponent {
-  displayedColumns: string[] = COLUMNS_SCHEMA.map((col) => col.key);
-  dataSource: any = USER_DATA;
-  columnsSchema: any = COLUMNS_SCHEMA;
+  displayedColumns: string[] = ['week', 'value'];
+  dataSource = ELEMENT_DATA;
+  clickedRows = new Set<PeriodicElement>();
 }
