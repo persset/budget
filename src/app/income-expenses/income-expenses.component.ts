35fb874ea32c447f86563bb4fcb43691e87be45d
Jose Victor Persset <jpersset@gmail.com>
2024-01-02 15:01:20 -0300

    Add: Create mini-form to add new incomes to the income table

--- src/app/income-expenses/income-expenses.component.ts
+++ src/app/income-expenses/income-expenses.component.ts
@@ -4,20 +4,25 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { HeaderComponent } from '../header/header.component';
 import { FormsModule } from '@angular/forms';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTableModule } from '@angular/material/table';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { SelectionModel } from '@angular/cdk/collections';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
-export interface PeriodicElement {
-  week: string;
+export interface Expense {
+  id: number;
+  name: string;
   value: number;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  { week: 'Week 1', value: 100 },
-  { week: 'Week 2', value: 100 },
-  { week: 'Week 3', value: 100 },
-  { week: 'Week 4', value: 100 },
+const ELEMENT_DATA: Expense[] = [
+  { id: 1, name: 'Name 1', value: 100 },
+  { id: 2, name: 'Name 2', value: 100 },
+  { id: 3, name: 'Name 3', value: 100 },
+  { id: 4, name: 'Name 4', value: 100 },
 ];
 @Component({
   selector: 'app-income-expenses',
@@ -31,12 +36,44 @@ const ELEMENT_DATA: PeriodicElement[] = [
     MatTableModule,
     MatButtonModule,
     MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
   ],
   templateUrl: './income-expenses.component.html',
   styleUrls: ['./income-expenses.component.css'],
 })
 export class IncomeExpensesComponent {
-  displayedColumns: string[] = ['week', 'value'];
-  dataSource = ELEMENT_DATA;
-  clickedRows = new Set<PeriodicElement>();
+  displayedColumns: string[] = ['select', 'name', 'value'];
+  dataSource = new MatTableDataSource<Expense>(ELEMENT_DATA);
+  selection = new SelectionModel<Expense>(true, []);
+
+  /** Whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  toggleAllRows() {
+    if (this.isAllSelected()) {
+      this.selection.clear();
+      return;
+    }
+
+    this.selection.select(...this.dataSource.data);
+  }
+
+  /** The label for the checkbox on the passed row */
+  checkboxLabel(row?: Expense): string {
+    if (!row) {
+      return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
+    }
+    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${
+      row.id + 1
+    }`;
+  }
+
+  addIncome() {}
 }
