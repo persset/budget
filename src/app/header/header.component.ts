db70c2f0b50dcf17835f20c833f7fa57756b1792
Jose Victor Persset <jpersset@gmail.com>
2024-01-03 14:51:22 -0300

    Add: Create router links for header components

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, RouterModule],
 })
 export class HeaderComponent {}
